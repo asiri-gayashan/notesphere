b49f311a396e999f1ebeff8842e80fdc6148cdee
asiri-gayashan <gayashanx@gmail.com>
2026-09-20 23:55:38 +0530

    Add text styles for consistent typography across the app

---
+++ lib/utils/text_styles.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:notesphere/utils/colors.dart';
+
+class AppTextStyles {
+  static const TextStyle appTitle = TextStyle(
+    fontSize: 28,
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle subTitle = TextStyle(
+    fontSize: 24,
+    color: Colors.white,
+    fontWeight: FontWeight.w500,
+  );
+  
+  static const TextStyle description = TextStyle(
+    fontSize: 20,
+    color: Colors.white,
+    fontWeight: FontWeight.w400,
+  );
+  
+  static const TextStyle descriptionSmall = TextStyle(
+    fontSize: 14,
+    color: Colors.white,
+    fontWeight: FontWeight.w400,
+  );
+  
+  
+  static const TextStyle appBody = TextStyle(
+    fontSize: 16,
+    color: Colors.white,
+  );
+  
+}
