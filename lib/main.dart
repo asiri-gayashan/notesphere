cf6ea91fe261540acb1ca2cff68ac3b2363c3056
asiri-gayashan <gayashanx@gmail.com>
2026-09-22 22:03:49 +0530

    Add Note and Todo models with Hive integration; update pubspec.yaml for dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notesphere/models/note_model.dart';
+import 'package:notesphere/models/todo_model.dart';
 import 'package:notesphere/utils/router.dart';
 import 'package:notesphere/utils/theme_data.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(NoteAdapter());
+  Hive.registerAdapter(TodoAdapter());
+
+  await Hive.openBox('notes');
+  await Hive.openBox('todos');
+
   runApp(const MyApp());
 }
 
@@ -16,9 +27,7 @@ class MyApp extends StatelessWidget {
       title: "Note Sphere",
       debugShowCheckedModeBanner: false,
       theme: ThemeClass.darkTheme.copyWith(
-        textTheme: GoogleFonts.openSansTextTheme(
-          Theme.of(context).textTheme,
-        ),
+        textTheme: GoogleFonts.openSansTextTheme(Theme.of(context).textTheme),
       ),
 
       routerConfig: AppRouter.router,
