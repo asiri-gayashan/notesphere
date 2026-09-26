3d548af91357de65bca9c149605e7ca15ac56abf
asiri-gayashan <gayashanx@gmail.com>
2026-09-26 23:21:35 +0530

    Remove AppRouter class and its routing definitions

--- lib/utils/router.dart
+++ lib/utils/router.dart
@@ -4,33 +4,4 @@ import 'package:notesphere/pages/home.dart';
 import 'package:notesphere/pages/notes_page.dart';
 import 'package:notesphere/pages/todo_page.dart';
 
-class AppRouter {
-  static final router = GoRouter(
-    navigatorKey: GlobalKey<NavigatorState>(),
-    debugLogDiagnostics: false,
-    initialLocation: "/",
-    routes: [
-      GoRoute(
-        name: "home",
-        path: "/",
-        builder: (context, state) {
-          return const HomePage();
-        },
-      ),
-      GoRoute(
-        name: "notes",
-        path: "/notes",
-        builder: (context, state) {
-          return const NotePage();
-        },
-      ),
-      GoRoute(
-        name: "todo",
-        path: "/todo",
-        builder: (context, state) {
-          return const ToDoPage();
-        },
-      ),
-    ],
-  );
-}
+
